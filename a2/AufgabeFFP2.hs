84c35cfc541df6880fad0e2e32c8e59b6cbf0d9f
th-b <thomas.baumhauer@gmail.com>
2013-04-08 13:57:25 +0200

    Teil 1,2 und 4

--- a2/AufgabeFFP2.hs
+++ a2/AufgabeFFP2.hs
@@ -1,13 +1,32 @@
 module AufgabeFFP2 where
 
 -- part 1: prime pairs
+primes :: [Integer]
+primes = sieve [2..]
+
+sieve :: [Integer] -> [Integer]
+sieve (x:xs) = x : sieve [y | y <- xs, mod y x > 0]
+
+doubleprimes :: [Integer]
+doubleprimes = [primes!!n | n <- [0..], (primes!!n) + 2 == primes!!(n+1)]
+
 pps :: [(Integer,Integer)]
-pps = (1,1) : pps
+pps = map makepair doubleprimes
+	where makepair n = (n, n+2)
 
 -- part 2: pow
+pow :: Int -> Integer
+pow 0 = 1
+pow n = pow (n-1) + pow(n-1)
+
 powFast :: Int -> Integer
-powFast 0 = 1
-powFast n = 1
+powFast n = powMemo!!n
+
+powMemo :: [Integer]
+powMemo = [powFromMemo n | n <- [0..]]
+	where powFromMemo m
+		| m == 0    = 1
+		| otherwise = powMemo!!(m-1) + powMemo!!(m-1)
 
 -- part 3: exp
 f :: Int -> Int -> Float
@@ -17,10 +36,15 @@ fMT :: Int -> Int -> Float
 fMT _ _ = 1.0
 
 -- part 4: Gödel numbers
+-- professional casting: int -> string -> char -> string -> int
+-- also gz 1 = gz 10, etc. would make more sense if digits were reversed...
 gz :: Integer -> Integer
-gz _ = 1
-
+gz n
+	| n < 0     = 0
+	| otherwise = foldr (*) 1 (zipWith (^) primes (map charToInt (show n)))
+		where charToInt c = read [c] :: Int
+	
 gzs :: [Integer]
-gzs =  1 : gzs
+gzs = map gz [1..]
 
 -- vim:sts=2:sw=2
