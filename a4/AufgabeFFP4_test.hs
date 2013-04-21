a62db23cc301f38cc35ccec2cc0d042c73387dc5
Gabriel Ebner <gebner@2b7e.org>
2013-04-21 09:55:43 +0200

    Fix argument order for calls to assertEqual.

--- a4/AufgabeFFP4_test.hs
+++ a4/AufgabeFFP4_test.hs
@@ -6,11 +6,11 @@ import Test.HUnit
 import AufgabeFFP4
 
 testknp1 = TestCase $ assertEqual ""
-  (knapsack [(2,3),(2,3),(3,4),(3,4),(5,6)] 10)
   ([(3,4),(3,4),(2,3),(2,3)],14)
+  (knapsack [(2,3),(2,3),(3,4),(3,4),(5,6)] 10)
 
 testbin1 = TestCase $ assertEqual ""
-  (binomDyn (4,2)) 6
+  6 (binomDyn (4,2))
 
 tests = TestList
   [ TestLabel "knapsack 1" testknp1
