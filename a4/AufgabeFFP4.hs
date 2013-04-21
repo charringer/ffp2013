14b43eeec94c9ec6a101d35af44f5590d3a778c9
Gabriel Ebner <gebner@2b7e.org>
2013-04-21 09:07:33 +0200

    Add DFS and stack code from the slides.

--- a4/AufgabeFFP4.hs
+++ a4/AufgabeFFP4.hs
@@ -1,5 +1,36 @@
 module AufgabeFFP4 where
 
+-- Stack {{{1
+
+data Stack a = EmptyStk | Stk a (Stack a)
+
+push :: a -> Stack a -> Stack a
+push x s = Stk x s
+
+pop :: Stack a -> Stack a
+pop (Stk _ s) = s
+pop EmptyStk = error "pop from an empty stack"
+
+top :: Stack a -> a
+top (Stk x _) = x
+top EmptyStk = error "top from an empty stack"
+
+emptyStack :: Stack a
+emptyStack = EmptyStk
+
+stackEmpty :: Stack a -> Bool
+stackEmpty (Stk _ _) = False
+stackEmpty EmptyStk = True
+
+-- DFS {{{1
+
+searchDfs :: (Eq node) => (node -> [node]) -> (node -> Bool) -> node -> [node]
+searchDfs succ goal x = search' (push x emptyStack)
+  where
+    search' s | stackEmpty s = []
+              | goal (top s) = top s : search' (pop s)
+	      | otherwise = let x = top s in search' (foldr push (pop s) (succ x))
+
 -- Knapsack Problem {{{1
 
 type Weight = Int
