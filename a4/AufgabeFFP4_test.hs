c28ebe11872ce99dfd6c83bbf949d2146ba95040
Claudio Harringer <der.claudio@gmail.com>
2013-05-13 14:05:31 +0200

    add test cases for exercise 4

--- a4/AufgabeFFP4_test.hs
+++ a4/AufgabeFFP4_test.hs
@@ -5,16 +5,26 @@ import Test.HUnit
 
 import AufgabeFFP4
 
-testknp1 = TestCase $ assertEqual ""
-  ([(3,4),(3,4),(2,3),(2,3)],14)
+testknp1 =
+  ([(3,4),(3,4),(2,3),(2,3)],14) ~=?
   (knapsack [(2,3),(2,3),(3,4),(3,4),(5,6)] 10)
 
-testbin1 = TestCase $ assertEqual ""
-  6 (binomDyn (4,2))
+testknpgrader1 = ([(2,3)],3) ~=? knapsack [(2,3)] 3
+
+testknpgrader2 = ([(2,3),(2,3)],6) ~=? knapsack [(2,3),(2,3)] 6
+
+testknpgrader3 =
+  let s[]=[];s(e:r)=s[i|i<-r,i<=e]++(e:s[i|i<-r,i>e])in
+    [] ~=? [s l|(l,14)<-[knapsack [(2,3),(2,3),(3,4),(3,4),(5,6)] 10],s l/= [(2,3),(2,3),(3,4),(3,4)]]
+
+testbin1 = 6 ~=? (binomDyn (4,2))
 
 tests = TestList
-  [ TestLabel "knapsack 1" testknp1
-  , TestLabel "binomial 1" testbin1
+  [ "knapsack 1"   ~: testknp1
+  , "knp grader 1" ~: testknpgrader1
+  , "knp grader 2" ~: testknpgrader2
+  , "knp grader 3" ~: testknpgrader3
+  , "binomial 1"   ~: testbin1
   ]
 
 main = do
