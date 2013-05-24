a8d8ab08406ebe531aef3d3d68c3ec7b683eb61f
Gabriel Ebner <gebner@gebner.org>
2013-05-24 13:03:31 +0200

    Make a9 work on hugs.

--- a9/AufgabeFFP9.hs
+++ a9/AufgabeFFP9.hs
@@ -1,5 +1,7 @@
 module AufgabeFFP9 where
 import Data.List
+import Test.QuickCheck
+import Data.Char
 
 type Text = String
 type Word = String
@@ -18,6 +20,12 @@ occS txt word = filterOverlaps $ initToIndex word $ filter (isSuffixOf word) $ i
 
 occI :: Text -> Word -> [(First,Last)]
 occI _ _ = []
+
+-- I'm really starting to get fed up with these ancient hugs libraries.
+-- And we can't even use CPP to make this work with GHC...
+instance Arbitrary Char where
+  arbitrary     = choose ('a', 'b')
+  coarbitrary c = variant (ord c `rem` 2)
 
 prop_coincide :: Text -> Word -> Bool
 prop_coincide txt word = occS txt word == occI txt word
