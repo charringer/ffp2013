76b55d954f260e648f9e35d738cc7e0c27357b0a
Gabriel Ebner <gebner@gebner.org>
2013-05-24 13:33:36 +0200

    Implement occI.

--- a9/AufgabeFFP9.hs
+++ a9/AufgabeFFP9.hs
@@ -21,7 +21,26 @@ occS :: Text -> Word -> [(First,Last)]
 occS txt word = endsToPairs word $ map length $ filter (isSuffixOf word) $ inits txt
 
 occI :: Text -> Word -> [(First,Last)]
-occI _ _ = []
+occI txt word = endsToPairs word $ test m $ scanl step (0, []) txt
+  where
+    test j [] =	[]
+    test j ((n, sx) : nxs)
+      | i == m      = n : test k (drop (k-1) nxs)
+      | m - k <= i  = test k (drop (k-1) nxs)
+      | otherwise   = test m (drop (k-1) nxs)
+      where i' = llcp sw (take j sx)
+	    i = if i' == j then m else i'
+	    k = shift sw i
+
+    (sw, m) = (reverse word, length word)
+
+    shift sw i = head [ k | k <- [1..m], llcp sw (drop k sw) == min i (m-k) ]
+
+step (n, sx) x = (n+1, x:sx)
+
+llcp xs [] = 0
+llcp [] ys = 0
+llcp (x:xs) (y:ys) = if x == y then 1 + llcp xs ys else 0
 
 -- I'm really starting to get fed up with these ancient hugs libraries.
 -- And we can't even use CPP to make this work with GHC...
@@ -30,4 +49,5 @@ instance Arbitrary Char where
   coarbitrary c = variant (ord c `rem` 2)
 
 prop_coincide :: Text -> Word -> Bool
+prop_coincide _ [] = True
 prop_coincide txt word = occS txt word == occI txt word
